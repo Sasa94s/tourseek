8bdda886d71761a9f66410210039cdcfebe8d65f
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2021-08-28 09:55:25 +0200

    DeleteRole API

--- tourseek_backend.api/Controllers/RolesController.cs
+++ tourseek_backend.api/Controllers/RolesController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using tourseek_backend.domain.Core;
 using tourseek_backend.domain.DTO.RoleDTOs;
 using tourseek_backend.domain.Entities;
+using tourseek_backend.repository.GenericRepository;
 using tourseek_backend.repository.UnitOfWork;
 using tourseek_backend.util.JsonResponses;
 
@@ -17,16 +18,18 @@ namespace tourseek_backend.api.Controllers
     {
         private readonly MappingProfile _mapper;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<ApplicationRole> _repository;
         public RolesController(IUnitOfWork unitOfWork)
         {
             _mapper = new MappingProfile();
             _unitOfWork = unitOfWork;
+            _repository = _unitOfWork.Repository<ApplicationRole>();
         }
 
         [HttpGet("{id}")]
         public ActionResult<ApplicationRole> GetRole(string id)
         {
-            var role = _unitOfWork.Repository<ApplicationRole>().GetById(id);
+            var role = _repository.GetById(id);
 
             if (role == null)
             {
@@ -46,12 +49,11 @@ namespace tourseek_backend.api.Controllers
 
         }
 
-
         [HttpPost]
         public ActionResult<ApplicationRole> CreateRole(RoleDto roleDto)
         {
             var role = _mapper.Mapper.Map<RoleDto, ApplicationRole>(roleDto);
-            var result = _unitOfWork.Repository<ApplicationRole>().Add(role);
+            var result = _repository.Add(role);
 
             if (!result)
             {
@@ -72,7 +74,7 @@ namespace tourseek_backend.api.Controllers
         [HttpPut("{id}")]
         public ActionResult<ApplicationRole> UpdateRole(UpdateRoleDto roleDto, string id)
         {
-            var role = _unitOfWork.Repository<ApplicationRole>().GetById(id);
+            var role = _repository.GetById(id);
 
             if (role == null)
             {
@@ -85,7 +87,7 @@ namespace tourseek_backend.api.Controllers
 
             _mapper.Mapper.Map(roleDto, role);
 
-            var result = _unitOfWork.Repository<ApplicationRole>().Update(role);
+            var result = _repository.Update(role);
 
             if (!result)
             {
@@ -101,6 +103,38 @@ namespace tourseek_backend.api.Controllers
                 StatusMessage = "Selected role has been updated.",
                 Success = true
 
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<ApplicationRole> DeleteRole(string id)
+        {
+            var selectedRole = _repository.GetById(id);
+
+            if (selectedRole == null)
+            {
+                return NotFound(new OtherJsonResponse
+                {
+                    StatusMessage = "Selected role not found.",
+                    Success = false
+                });
+            }
+
+            var result = _repository.Remove(selectedRole);
+            if (!result)
+            {
+                return BadRequest(new OtherJsonResponse
+                {
+                    StatusMessage = "Couldn't delete selected role.",
+                    Success = false
+
+                });
+            }
+
+            return Ok(new OtherJsonResponse
+            {
+                StatusMessage = "Role has been deleted successfully.",
+                Success = true
             });
         }
     }
