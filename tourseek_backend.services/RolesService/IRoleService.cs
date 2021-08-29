0c62b4d4d8344196f8066b6f38eb32ee2bd7ebce
Mostafa Elsheikh <abdelaleem@gmx.com>
2021-08-29 17:54:55 +0200

    chore: extract RoleService methods to interface

--- tourseek_backend.services/RolesService/IRoleService.cs
+++ tourseek_backend.services/RolesService/IRoleService.cs
@@ -1,6 +1,12 @@
-﻿namespace tourseek_backend.services.RolesService
+﻿using tourseek_backend.domain.DTO.RoleDTOs;
+
+namespace tourseek_backend.services.RolesService
 {
     public interface IRoleService
     {
+        RoleDto GetRoleById(string id);
+        string CreateRole(RoleDto roleDto);
+        string UpdateRole(UpdateRoleDto roleDto, string id);
+        string DeleteRole(string id);
     }
 }
